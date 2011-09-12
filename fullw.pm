a5c997ff4f51cb048f7276a8e73c71a027f59202
BEN ENGLISCH <EMAILBEN145@gmail.com>
2011-09-11 22:47:55 -0500

    where did these tabs come from.

--- fullw.pm
+++ fullw.pm
@@ -3,15 +3,15 @@ use base 'Exporter';
 our @EXPORT = qw( fullw );
 
 sub fullw {
-	my $DELTA = 65248;
-	my $str   = shift;
+    my $DELTA = 65248;
+    my $str   = shift;
 
-	return join "", map {
-		if ( $_ eq " " ) {
-			"\x{3000}";
-		}
-		else {
-			chr( ord($_) + $DELTA );
-		}
-	} split( //, $str );
+    return join "", map {
+        if ( $_ eq " " ) {
+            "\x{3000}";
+        }
+        else {
+            chr( ord($_) + $DELTA );
+        }
+    } split( //, $str );
 }
